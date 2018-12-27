154f4f20681bc77e5c14ad8455f97851e9713dba
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-12-27 22:05:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,7 +76,9 @@ class Student
           LIMIT 1
           SQL
 
-          DB[:conn].execute(sql, 10)
+          DB[:conn].execute(sql, 10).map do |row|
+              new_from_db(row)
+          end.first
   end
 
   def save
