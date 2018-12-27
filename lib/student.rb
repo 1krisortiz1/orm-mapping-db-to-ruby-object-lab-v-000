924fb5a5d2ab345eb5baad35de6532a0ae54f3a4
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-12-27 21:21:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,12 @@ class Student
   end
 
   def self.all_students_in_grade_9
+      sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade.size = 9
+      SQL
+      DB[:conn].execute(sql)
   end
 
   def save
